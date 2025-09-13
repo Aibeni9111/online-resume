a73b513865932d269fd59262affe8c636d149653
Aibek Taalaibekov <taa211087@spengergasse.at>
2025-09-13 20:49:19 +0200

    feat(backend): add Spring Boot skeleton (resume-api)

---
+++ backend/resume-api/src/test/java/com/aibeni/resume/ResumeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibeni.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
